344cc45540ab9055e7a7061bfcf2c1db19365b6a
BillStephens2022 <two4onebill@yahoo.com>
2023-02-27 17:33:59 -0500

    refactored  to useContext instead of props, but need to debug

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,20 @@
-import BookShow from './BookShow';
+import { useContext } from "react";
+import BooksContext from "../context/books";
+import BookShow from "./BookShow";
 
-function BookList({ books, onDelete, onEdit }) {
+function BookList() {
+  const { books } = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
-    return <BookShow key={book.id} book={book} onEdit={onEdit} onDelete={onDelete} />
+    return (
+      <BookShow key={book.id} book={book} />
+    );
   });
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {renderedBooks}
+    </div>
+  );
 }
 
 export default BookList;
